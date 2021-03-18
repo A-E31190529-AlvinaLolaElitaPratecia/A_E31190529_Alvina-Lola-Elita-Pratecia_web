eb12fa2afabf033c4bd551bbae1e174f8b4ae58d
A-E31190529-AlvinaLolaElitaPratecia <alvinalolaelitapratecia@gmail.com>
2021-03-18 11:47:01 +0700

    Minggu 7

---
+++ Minggu 7/Codeigniter/application/models/Hello_model.php
@@ -0,0 +1,14 @@
+<?php
+class Hello_model extends CI_Model{
+    //public $txt = 'Hello World!';
+    
+    public $alert = 'Hello Wolrd dari CI Model';
+    
+    public $mahasiswa = [
+			["nama"=>"Alvina","nim"=>"E31190529","gol"=>"A"],
+			["nama"=>"Lola","nim"=>"E31190529","gol"=>"A"],
+			["nama"=>"Elita","nim"=>"E31190529","gol"=>"A"],
+			["nama"=>"Pratecia","nim"=>"E31190529","gol"=>"A"]
+		];
+}
+?>
---
+++ Minggu 7/Codeigniter/application/views/contact.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+</head>
+<body>
+	<h2>Halaman Contact</h2>
+	<p>Selamat datang ini adalah halaman contact</p>
+</body>
+</html>
