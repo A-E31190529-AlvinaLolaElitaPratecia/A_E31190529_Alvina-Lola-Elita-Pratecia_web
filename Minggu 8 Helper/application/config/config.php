494066b710a74273673f2aeaeee9eaa3e535ffd9
A-E31190529-AlvinaLolaElitaPratecia <alvinalolaelitapratecia@gmail.com>
2021-04-13 11:04:35 +0700

    Update config.php

--- Minggu 8 Helper/application/config/config.php
+++ Minggu 8 Helper/application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://localhost/Codeigniter';
+$config['base_url'] = 'http://localhost/A_E31190529_Alvina-Lola-Elita-Pratecia_web/Minggu 8 Helper';
 
 /*
 |--------------------------------------------------------------------------
