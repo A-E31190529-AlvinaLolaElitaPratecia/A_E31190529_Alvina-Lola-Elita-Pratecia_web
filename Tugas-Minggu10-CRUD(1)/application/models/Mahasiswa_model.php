149d4a91cadfe4cab5eb1d0ec9d6eb396de6a09a
A-E31190529-AlvinaLolaElitaPratecia <alvinalolaelitapratecia@gmail.com>
2021-06-07 09:42:03 +0700

    CRUD-2

--- Tugas-Minggu10-CRUD(1)/application/models/Mahasiswa_model.php
+++ Tugas-Minggu10-CRUD(1)/application/models/Mahasiswa_model.php
@@ -10,4 +10,27 @@ public function getAll(){
 
 public function input_data($data,$table){
 	$this->db->insert($table,$data);
-}}?>
+}
+function edit_data($where,$table) {
+		return $this->db->get_where($table, $where);
+	}
+
+	function update_data($where,$data,$table) {
+		$this->db->where($where);
+		$this->db->update($table, $data);
+	}
+
+	function hapus_data($where, $table) {
+		$this->db->where($where);
+		$this->db->delete($table);
+	}
+
+	function login($user, $pass, $table){
+		$this->db->select('*');
+		$this->db->from('tm_user');
+		$this->db->where('username', $user);
+		$this->db->where('password', $pass);
+		$query = $this->db->get();
+		return $query;
+	}
+}?>
